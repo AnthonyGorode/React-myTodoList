7940da21f4dbede154146fd76a3f0d773927a1e6
anthony gorode <anthony.gorode@gmail.com>
2019-08-27 05:58:09 +1100

    Commit 2 : installation de react-redux, mis en place du store avec les actions & les reducers, utilisation du Provide pour rendre le store disponible dans toute l'application, utilisation de connect pour connecter des components container au store (TodoList,AddTodo,Filter)

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
-const TodoItem = props => {
+const TodoItem = ({todo, toggleTodo, deleteTodo}) => {
     return (
-        <li className="list-group-item d-flex flex-row justify-content-between align-items-center list-group-item-action" >
-            <span> x </span>
+        <li onClick={ toggleTodo } className="list-group-item d-flex flex-row justify-content-between align-items-center list-group-item-action" >
+            <span> { todo.name } </span>
             <span>
-                <input className="mx-3"  type="checkbox" />
-                <button className="btn btn-sm btn-danger">delete</button>
+                <input 
+                    checked={ todo.done } 
+                    onChange={ () => {} } 
+                    className="mx-3"  
+                    type="checkbox" 
+                />
+                <button onClick={ (event) => {
+                    event.stopPropagation();
+                    deleteTodo();
+                } } className="btn btn-sm btn-danger">delete</button>
             </span>
         </li>
     );
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import * as reducers from './reducers';
+import { combineReducers } from 'redux';
+import { createStore } from 'redux';
+
+const todoReducers = combineReducers(reducers);
+const store = createStore(todoReducers);
+
+export default store;
+
