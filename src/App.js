dfa86f0882b7f225832619a997858787b64de7ad
anthony gorode <anthony.gorode@gmail.com>
2019-08-26 21:41:06 +1100

    Commit 1 : Création de la todo list dans le component App, séparation de la logique par component (AddTodo,TodoList,TodoItem,Filter)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
+import Filter from './components/Filter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container p-5">
+        <h4>Ajouter une todo</h4>
+        <hr className="my-4" />
+        <AddTodo />
+        <hr className="my-4" />
+        <div className="card">
+          <div className="card-header d-flex flex-row align-items-center">
+            <span className="flex-fill">Todo list</span>
+            <Filter />
+          </div>
+          <div className="card-body">
+            <TodoList /> 
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
